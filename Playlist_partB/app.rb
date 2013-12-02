e83a72b32b6dff50174d345e72a00428d72adc40
ajosepha <ariel.isaacs@gmail.com>
2013-12-02 10:09:48 -0500

    modify app and parser

--- Playlist_partB/app.rb
+++ Playlist_partB/app.rb
@@ -4,13 +4,19 @@ require 'parser'
 
 class Playlist
 
+    def initialize
+        parser = Parser.new
+        @artists = parser.artists
+        @genres = parser.genres
+        @songs = parser.songs
+    end
+
 
     def introduction
         puts "Type genre to browse by genre, or artist to browse by artist"
         response = gets.chomp
             if response = genre
                 ap Genres.self.all
-
     end
 
 
